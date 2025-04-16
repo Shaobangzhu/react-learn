59568773bd1773c727ca4e9c5e055fd61cc215de
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-16 10:17:00 -0700

    useState principle explaination

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 
-// trigger -> render -> commit
+// 1. trigger -> render(virtual dom) -> commit
+// 2. virtual dom: 真实dom的一个js对象的表达
+// 3. 快照态的数据
+// 4. bacthUpdate
+
 function App() {
   const [ count, setCount ] = useState(0);
   
@@ -9,11 +13,20 @@ function App() {
   // const setCount = result[1];
 
   function handleClick() {
-    setCount(count + 1);
+    // setCount(count + 1);
+    // setCount((count) => count + 1);
+    // setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  }
+
+  function handleInnerClick() {
+    alert('inner click');
   }
 
   return (
-    <div onClick={handleClick}>{count}</div>
+    <div onClick={handleClick}>
+      <p onClick={handleInnerClick}>{count}</p>
+    </div>
   )
 }
 
