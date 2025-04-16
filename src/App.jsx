8c040d4fb8e60bee37ff04e4daac8166809a3ce8
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-16 15:55:00 -0700

    add comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { useImmer } from 'use-immer';
 // 命令式: 你一步步指路,告诉出租车司机怎么回家
 // 声明式: 你告诉出租车司机,你家在哪, 出租车司机会自动把车开到家门口
 
+// 1. 避免数据冗余重复
+// 2. 同类型数据尽量合并
+// 3. 数据结构能浅不深
+
 function App() {
   const [user, setUser] = useImmer({
     firstName: '',
