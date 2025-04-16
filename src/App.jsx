8106ffd0c7956f55e6977884b84e000473453b6e
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-16 15:40:57 -0700

    Declarative Programming Demo with string input

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useImmer } from "use-immer";
+import { useState } from 'react';
 
-// immutable 编程规范
+// 从命令式开发到声明式开发
+// 命令式: 你一步步指路,告诉出租车司机怎么回家
+// 声明式: 你告诉出租车司机,你家在哪, 出租车司机会自动把车开到家门口
 
 function App() {
-  const [data, setData] = useImmer({
-    count: 0
-  });
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  function handleClick() {
-    setData((draft) => {
-      draft.count = draft.count + 1;
-    });
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  return <div onClick={handleClick}>{data.count}</div>;
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <div>
+      First Name: <input onChange={handleFirstNameChange} />
+      <br/>   
+      <br/>  
+      Last Name: <input onChange={handleLastNameChange} />
+      <br/>   
+      <br/> 
+      Full Name: {firstName} {lastName}
+    </div>)
 }
 
 export default App;
