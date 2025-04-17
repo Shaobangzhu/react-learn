82aee6ccefe935f687ba6bfeec2f2bd79cc654a6
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-16 17:17:46 -0700

    change the project default port to 4000

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import { PartOne, PartTwo } from './Parts';
+import Counter from './Counter';
 
-// 父子组件传值, 主要是为了解决组件间数据共享的问题
 function App() {
-  const [ showPartOne, setShowPartOne ] = useState(true);
 
   return (
     <div>
-      <PartOne showPartOne={showPartOne} setShowPartOne={setShowPartOne} />
-      <PartTwo showPartOne={showPartOne} setShowPartOne={setShowPartOne} />
+      <Counter />
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div onClick={() => setCount(count + 1)}>
+        {count}
+    </div>
+  );
+}
+
+export default Counter;
