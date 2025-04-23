8474960903ea9f3094992c8de46a9c0d1da15e1b
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-22 21:43:39 -0700

    sep reducer to another file

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,47 @@
-import { useState, useReducer } from "react";
-// reducer 使用流程
-// 1. 定义数据
-// 2. 定义 reducer 函数
-// 3. 定义 Action 发送改变数据的指令
-// 4. 拿到 Action 之后, dispatch 派发 Action
-// 5. Reducer中 根据指令修改数据
-// 6. 完成数据的修改
+import { useReducer } from "react";
+import dataReducer from "./reducer";
 
-function listReducer(state, action) {
-  if (action.type === "add") {
-    const newState = [
-      ...state,
-      {
-        id: action.value,
-        value: action.value,
-      },
-    ];
-    return newState;
-  }
-  if (action.type === "delete") {
-    const newState = [...state];
-    newState.splice(action.value, 1);
-    return newState;
-  }
-  return state;
-}
+// 使用Reducer带来的变化
+// 1. 业务逻辑代码量显著降低
+// 2. 代码可读性比较高
+// 3. 前端自动化测试方便 Jest
+// 4. 组件复用性降低
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [list, dispatch] = useReducer(listReducer, []);
+  const [data, dispatch] = useReducer(dataReducer, {
+    inputValue: '',
+    list: []
+  });
 
   function handleInputChange(e) {
-    setInputValue(e.target.value);
+    const action = { type: 'changeInput', value: e.target.value };
+    dispatch(action);
   }
 
   function handleButtonClick() {
-    const action = {
-      type: "add",
-      value: inputValue,
-    };
+    const action = { type: 'addItem' }
     dispatch(action);
-    setInputValue("");
   }
 
   function handleItemClick(index) {
-    const action = {
-      type: "delete",
-      value: index,
-    };
+    const action = { type: "deleteItem", value: index };
     dispatch(action);
   }
 
   return (
     <div>
       <div>
-        <input value={inputValue} onChange={handleInputChange} />
+        <input value={data.inputValue} onChange={handleInputChange} />{"  "}
         <button onClick={handleButtonClick}>Submit</button>
       </div>
       <ul>
-        {list.map((item, index) => (
-          <li key={item.id} onClick={() => handleItemClick(index)}>
-            {item.value}
-          </li>
-        ))}
+        {
+          data.list.map((item, index) => (
+            <li key={item.id} onClick={() => handleItemClick(index)}>
+              {item.value}
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
