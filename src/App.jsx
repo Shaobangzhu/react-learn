04439baf27a3f336bd28736f60c57997d003c9e7
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-23 11:33:24 -0700

    使用Context完成深层组件传值

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import { useReducer } from "react";
-import dataReducer from "./reducer";
-
-// 使用Reducer带来的变化
-// 1. 业务逻辑代码量显著降低
-// 2. 代码可读性比较高
-// 3. 前端自动化测试方便 Jest
-// 4. 组件复用性降低
+import Header from "./Header/index";
+import Body from "./Body/index";
+import nameContext from "./nameContext";
 
 function App() {
-  const [data, dispatch] = useReducer(dataReducer, {
-    inputValue: '',
-    list: []
-  });
-
-  function handleInputChange(e) {
-    const action = { type: 'changeInput', value: e.target.value };
-    dispatch(action);
-  }
-
-  function handleButtonClick() {
-    const action = { type: 'addItem' }
-    dispatch(action);
-  }
-
-  function handleItemClick(index) {
-    const action = { type: "deleteItem", value: index };
-    dispatch(action);
-  }
-
   return (
     <div>
-      <div>
-        <input value={data.inputValue} onChange={handleInputChange} />{"  "}
-        <button onClick={handleButtonClick}>Submit</button>
-      </div>
-      <ul>
-        {
-          data.list.map((item, index) => (
-            <li key={item.id} onClick={() => handleItemClick(index)}>
-              {item.value}
-            </li>
-          ))
-        }
-      </ul>
+      <nameContext.Provider value='clu'>
+        <Header />
+        <Body />
+      </nameContext.Provider>
     </div>
   );
 }
---
+++ src/Body/Content.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import nameContext from "../nameContext";
+
+function Content() {
+  const name = useContext(nameContext);
+
+  return (
+    <div
+      style={{
+        marginLeft: '100px'
+      }}
+    >
+      Content: {name}
+    </div>
+  );
+}
+
+export default Content;
