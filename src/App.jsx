6da8d54e3a6eb17e0dbdc95f1a7d8c6739558c54
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-26 19:20:13 -0700

    4-4 useEffect的底层执行逻辑

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useRef } from 'react';
-import InputComponent from './inputComponent';
+// 2. Effect 是在 render 执行结束, 页面更新之后, 再执行的
+import { useState } from 'react';
+import VideoPlayerTwo from './VideoPlayerTwo';
 
 function App() {
-  const inputElement = useRef(null);
+  // 播放状态
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function handleBtnClick() {
-    inputElement.current.focus();
+    setIsPlaying(!isPlaying);
   }
+
+  const btnContent = isPlaying ? "Pause" : "Play";
 
   return (
     <div>
-      <InputComponent ref={ inputElement } />{" "}
-      <button onClick={handleBtnClick}>Focus</button>
+      <VideoPlayerTwo src="/video/a.mp4" isPlaying={isPlaying} />
+      <button onClick={handleBtnClick}>{btnContent}</button>
     </div>
   );
 }
