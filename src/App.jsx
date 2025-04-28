e5d9391ca9047feb3bd2e61a908f9fd6817cd4b3
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-27 18:55:50 -0700

    timer 在严格模式下, 需要进行清理

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-// 2. Effect 是在 render 执行结束, 页面更新之后, 再执行的
-import { useState } from 'react';
-import VideoPlayerTwo from './VideoPlayerTwo';
+// Strict Mode 严格模式, 在 Effect 中的执行逻辑
+// render 函数渲染完成后, useEffect 会开始判断是否执行
+import { useState, useEffect } from "react";
 
 function App() {
-  // 播放状态
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [time, setTime] = useState(new Date().getTime());
+  
+  // timer 在严格开发模式下, 需要进行清理
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(123);
+      setTime((new Date()).getTime());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
 
-  function handleBtnClick() {
-    setIsPlaying(!isPlaying);
-  }
-
-  const btnContent = isPlaying ? "Pause" : "Play";
-
-  return (
-    <div>
-      <VideoPlayerTwo src="/video/a.mp4" isPlaying={isPlaying} />
-      <button onClick={handleBtnClick}>{btnContent}</button>
-    </div>
-  );
+  return <div>{time}</div>;
 }
 
 export default App;
