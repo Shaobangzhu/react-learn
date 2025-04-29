ba52aaac3b5ee445f993c8b20a84b7f0ba98d4d2
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-29 16:02:29 -0700

    prevent memory leak

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// 如果一个数据内容是来自于外部系统的, 使用 useSyncExternalState 来实现
 function App() {
   const [isOnline, setIsOnline] = useState(true);
 
@@ -10,6 +11,10 @@ function App() {
   useEffect(() => {
     window.addEventListener("online", updateState);
     window.addEventListener("offline", updateState);
+    return () => {
+      window.removeEventListener("online", updateState);
+      window.removeEventListener("offline", updateState);
+    };
   }, []);
 
   return <div>{isOnline.toString()}</div>;
