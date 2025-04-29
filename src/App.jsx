c17f0be1ed9c67ac02c363a0fd40a77f6e50ed35
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-28 22:40:11 -0700

    useMemo的使用场景和方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-// Strict Mode 严格模式, 在 Effect 中的执行逻辑
-// render 函数渲染完成后, useEffect 会开始判断是否执行
-import { useState, useEffect } from "react";
+import { useState, useMemo } from 'react';
 
 function App() {
-  const [time, setTime] = useState(new Date().getTime());
-  
-  // timer 在严格开发模式下, 需要进行清理
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     console.log(123);
-  //     setTime((new Date()).getTime());
-  //   }, 1000);
-  //   return () => {
-  //     clearInterval(timer);
-  //   }
-  // }, []);
+  const list = ['1. Long Call', '2. Long Put', '3. Covered Call', '4. Call Spread'];
+  const [name, setName] = useState('');
+  const [search, setSearch] = useState('');
 
-  // 全局事件绑定在严格开发模式下, 需要进行清理
-  // useEffect(() => {
-  //   function onScroll() {
-  //     console.log('scroll');
-  //   }
-  //   window.addEventListener('scroll', onScroll);
+  console.log('step 1');
 
-  //   return () => {
-  //     window.removeEventListener('scroll', onScroll);
-  //   }
-  // }, []);
+  // useMemo 相当于做了一个缓存
+  // useEffect 是在 render 结束后再执行的
+  // useMemo 是在render 运行的过程中执行的
+  const filteredList = useMemo(() => {
+    console.log('step 2');
+    return list.filter(item => item.toLowerCase().includes(search.toLowerCase()));
+    // eslint-disable-next-line
+  }, [search]);
 
-  // DOM 操作在严格模式下, 需要进行清理
-  useEffect(() => {
-    const originBackground = window.document.body.style.background;
-    window.document.body.style.background = 'red';
-    return () => {
-      window.document.body.style.background = originBackground;
-    }
-  }, []);
+  console.log('step 3');
 
-  return <div style={{height: '5000px'}}>{time}</div>;
+  return (
+    <div>
+      Name: <input value={name} onChange={(e)=>{setName(e.target.value)}} /> <br />
+      <br />
+      Search: <input value={search} onChange={(e)=>{setSearch(e.target.value)}} /> <br />
+      <br />
+      <select>
+        {filteredList.map(item => {
+          return <option key={item}>{item}</option>
+        })}
+      </select>
+    </div>
+  );
 }
 
 export default App;
