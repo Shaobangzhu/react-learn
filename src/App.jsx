8a6e9143b7b2f0b69d8b59ad61f7d76458d15610
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-08 18:37:13 -0700

    useEffectEvent的简介

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-// 1. 组件存在的整个过程称为一个生命周期
-// 挂载(mount) -> 更新(update) -> 卸载(unmount)
-// 组件的挂载和销毁只有一次
-// 2. Effect 存在的整个过程称为一个生命周期
-// 创建执行 -> 销毁
-// 组件里的 effect 的挂载和销毁可能会有很多次
-// 3. Effect和Event的区别是什么呢?
-// Effect是有依赖数据驱动执行的, Event一定是由用户操作行为驱动执行的
-
-import { useState, useEffect } from "react";
+// useEffectEvent
+import { useState, useEffect, useEffectEvent } from "react";
 
 function App() {
-  const [username, setUsername] = useState("clu");
+
+  const [url, setUrl] = useState('http://localhost:3000')
+  const [param, setParam] = useState('?name=clu')
+
+  const request = useEffectEvent((url) => {
+    console.log(`发送请求, 地址是${url}${param}`)
+  }) 
 
   useEffect(() => {
-    console.log("execute");
-    return () => {
-      console.log("destroy");
-    };
-  }, [username]);
-
-  console.log("render");
+    request(url)
+  }, [url])
 
   return (
-    <div
-      onClick={() => {
-        setUsername("CLU");
-      }}
-    >
-      HELLO REACT
-    </div>
-  );
+    <>
+      <div onClick={()=>{setUrl('http://localhost:3001')}}>Change Url</div>
+      <div onClick={()=>{setParam('?name=CLU')}}>Change Param</div>
+    </>
+  )
 }
 
 export default App;
