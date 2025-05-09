38c993a31caae16e69a140b76ede3c2d862fefb4
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-08 17:40:22 -0700

    组件和Effect的生命周期

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { useSyncExternalStore } from "react";
+// 1. 组件存在的整个过程称为一个生命周期
+// 挂载(mount) -> 更新(update) -> 卸载(unmount)
+// 组件的挂载和销毁只有一次
+// 2. Effect 存在的整个过程称为一个生命周期
+// 创建执行 -> 销毁
+// 组件里的 effect 的挂载和销毁可能会有很多次
+// 3. Effect和Event的区别是什么呢?
+// Effect是有依赖数据驱动执行的, Event一定是由用户操作行为驱动执行的
 
-// 如果一个数据内容是来自于外部系统的, 使用 useSyncExternalState 来实现
-function subscribe(callback) {
-  window.addEventListener("online", callback);
-  window.addEventListener("offline", callback);
-  return () => {
-    window.removeEventListener("online", callback);
-    window.removeEventListener("offline", callback);
-  };
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const isOnline = useSyncExternalStore(subscribe, () => window.navigator.onLine);
+  const [username, setUsername] = useState("clu");
 
-  return <div>{isOnline.toString()}</div>;
+  useEffect(() => {
+    console.log("execute");
+    return () => {
+      console.log("destroy");
+    };
+  }, [username]);
+
+  console.log("render");
+
+  return (
+    <div
+      onClick={() => {
+        setUsername("CLU");
+      }}
+    >
+      HELLO REACT
+    </div>
+  );
 }
 
 export default App;
