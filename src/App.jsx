982707c092ef189e4677459deeeb8c6f504dbfb5
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-11 18:31:41 -0700

    Profiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useState, useRef, forwardRef, useEffect, useImperativeHandle } from "react";
-
-const UserInput = forwardRef((props, ref) => {
-  const inputRef = useRef(null);
-
-  // 父组件调用子组件的  DOM 时, 能够对DOM节点上的返回内容中转做一层限制
-  useImperativeHandle(ref, () => {
-    return {
-      blur() {
-        inputRef.current.blur();
-      },
-      value: inputRef.current.value
-    }
-  }, [])
-
-  const[ value, setValue ] = useState('clu');
-  return <input ref={inputRef} value={value || ''} onChange={(e)=>{setValue(e.target.value)}} />
-});
-
 function App() {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    console.log(ref.current.value)
-  }, [])
 
   return (
-    <UserInput ref={ref} />
+    <div>Hello React</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { Profiler } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function renderCallback(
+  id,
+  phase,
+  actualDuration,
+  baseDuration,
+  startTime,
+  commitTime
+) {
+  console.log(id, phase, actualDuration, baseDuration, startTime, commitTime);
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Profiler id="app" onRender={renderCallback}>
+      <App />
+    </Profiler>
   </React.StrictMode>
 );
