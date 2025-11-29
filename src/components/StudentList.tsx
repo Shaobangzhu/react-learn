c9109971522b55db5a72a69f33311f187af84b09
Chaoran Lu <andyluloveusc@gmail.com>
2025-11-29 00:13:03 -0800

    3. List Rendering + key

---
+++ src/components/StudentList.tsx
@@ -0,0 +1,19 @@
+// 3. 列表渲染 + key (学生列表)
+// 写一个<StudentList />: 接收students prop, 渲染<li>列表, 使用稳定的key
+// Props: 组件从父组件接收的数据
+
+type Student = { id: number; name: string };
+
+interface StudentListProps {
+    students: Student[];
+}
+
+export function StudentList({ students }: StudentListProps) {
+    return (
+        <ul>
+            {students.map((s) => (
+                <li key={s.id}>{s.name}</li>
+            ))}
+        </ul>
+    );
+}
